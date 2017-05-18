7fabcc77895e161448806a8d06ce16b4f3480b39
Zac Berkowitz <zac.berkowitz@gmail.com>
2017-05-18 15:49:18 +0000

    Rustfmt

--- tests/test_unix.rs
+++ tests/test_unix.rs
@@ -58,21 +58,25 @@ fn test_serial_pair() {
             "Invalid file descriptor on master ptty");
     assert!(slave.as_raw_fd() > 0,
             "Invalid file descriptor on slave ptty");
-    assert_ne!(master.as_raw_fd(), slave.as_raw_fd(),
-            "master and slave ptty's share the same file descriptor.");
+    assert_ne!(master.as_raw_fd(),
+               slave.as_raw_fd(),
+               "master and slave ptty's share the same file descriptor.");
 
     let msg = "Test Message";
     let mut buf = [0u8; 128];
 
     // Write the string on the master
-    assert_eq!(master.write(msg.as_bytes()).unwrap(), msg.len(),
-            "Unable to write message on master.");
+    assert_eq!(master.write(msg.as_bytes()).unwrap(),
+               msg.len(),
+               "Unable to write message on master.");
 
     // Read it on the slave
     let nbytes = slave.read(&mut buf).expect("Unable to read bytes.");
-    assert_eq!(nbytes, msg.len(),
-            "Read message length differs from sent message.");
+    assert_eq!(nbytes,
+               msg.len(),
+               "Read message length differs from sent message.");
 
-    assert_eq!(str::from_utf8(&buf[..nbytes]).unwrap(), msg,
-            "Received message does not match sent");
+    assert_eq!(str::from_utf8(&buf[..nbytes]).unwrap(),
+               msg,
+               "Received message does not match sent");
 }
