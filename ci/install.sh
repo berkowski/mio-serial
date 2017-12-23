ee6e120dce5d3cbef944432dc5163aef7b535d4f
Zac Berkowitz <zberkowitz@whoi.edu>
2017-12-23 12:19:58 -0500

    Updated trust CI

--- ci/install.sh
+++ ci/install.sh
@@ -1,31 +1,53 @@
 set -ex
 
 main() {
-    curl https://sh.rustup.rs -sSf | \
-        sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
-
-    local cross_ver=v0.1.10
-
     local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
-        target=x86_64-unknown-linux-gnu
+        target=x86_64-unknown-linux-musl
+        sort=sort
     else
         target=x86_64-apple-darwin
+        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
 
-    # TODO At some point you'll probably want to use a newer release of `cross`,
-    # simply change the argument to `--tag`.
+    # Builds for iOS are done on OSX, but require the specific target to be
+    # installed.
+    case $TARGET in
+        aarch64-apple-ios)
+            rustup target install aarch64-apple-ios
+            ;;
+        armv7-apple-ios)
+            rustup target install armv7-apple-ios
+            ;;
+        armv7s-apple-ios)
+            rustup target install armv7s-apple-ios
+            ;;
+        i386-apple-ios)
+            rustup target install i386-apple-ios
+            ;;
+        x86_64-apple-ios)
+            rustup target install x86_64-apple-ios
+            ;;
+    esac
+
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
-           --tag $cross_ver \
+           --tag $tag \
            --target $target
 
-    # Build custom linux docker images
-    if [ $TARGET != ${TARGET#*linux} ]; then
-	docker build -t mio-serial/$TARGET ci/docker/$TARGET
-    fi
+   # Build custom linux docker images
+   if [ $TARGET != ${TARGET#*linux} ]; then
+      docker build -t mio-serial/$TARGET ci/docker/$TARGET
+   fi
+
 }
 
 main
