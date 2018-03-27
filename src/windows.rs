62d4baeb248acc19ee48fd9a7cefe8a104fe3a7e
James Willcox <snorp@snorp.net>
2018-03-27 17:57:24 -0500

    Build against serialport 2.3.0

--- src/windows.rs
+++ src/windows.rs
@@ -265,6 +265,10 @@ impl SerialPort for Serial {
     fn read_carrier_detect(&mut self) -> serialport::Result<bool> {
         self.inner.read_carrier_detect()
     }
+
+    fn try_clone(&self) -> ::Result<Box<SerialPort>> {
+        panic!("try_clone() is not supported");
+    }
 }
 
 impl Read for Serial {
