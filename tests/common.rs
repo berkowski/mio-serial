460d343684640f25c27f56c3da1d7a099ccddb4e
Paolo Barbolini <paolo.barbolini@m4ss.net>
2026-06-12 18:21:55 +0200

    Avoid using `String::leak` in tests (#55)
    * Fix rustfmt and clippy warnings
    
    * Avoid using `String::leak` in tests
    
    ---------
    
    Co-authored-by: Eric Stokes <eestokes@pm.me>

--- tests/common.rs
+++ tests/common.rs
@@ -177,8 +177,8 @@ pub fn checked_read(port: &mut mio_serial::SerialStream, data: &mut [u8], expect
 pub struct Fixture {
     #[cfg(unix)]
     process: process::Child,
-    pub port_a: &'static str,
-    pub port_b: &'static str,
+    pub port_a: String,
+    pub port_b: String,
 }
 
 #[cfg(unix)]
@@ -202,8 +202,8 @@ impl Fixture {
         static N: AtomicUsize = AtomicUsize::new(0);
         LOGGING_INIT.call_once(env_logger::init);
         let n = N.fetch_add(1, Ordering::Relaxed);
-        let port_a = format!("{port_a}{n}").leak();
-        let port_b = format!("{port_b}{n}").leak();
+        let port_a = format!("{port_a}{n}");
+        let port_b = format!("{port_b}{n}");
         let args = [format!("PTY,link={port_a}"), format!("PTY,link={port_b}")];
         log::trace!("starting process: socat {} {}", args[0], args[1]);
 
